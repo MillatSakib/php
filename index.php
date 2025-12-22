bd1394b88b1d05a757327abd4f4e4c0da0920b03
Md. Sohan Millat Sakib <me@millatsakib.com>
2025-12-22 23:28:02 +0600

    Form submission code added

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<html>
 <body>
-    <?php
-    echo 'This is my first project'; ?>
+
+<?php
+$color = "red";
+echo "My car is $color<br>";
+echo "My house is $COLOR";
+?>
+
 </body>
 </html>
